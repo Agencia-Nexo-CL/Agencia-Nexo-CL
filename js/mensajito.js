977df798bb3a661e4384aa856d3ff0d64de19280
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2023-12-15 11:16:18 -0300

    more changes

--- js/mensajito.js
+++ js/mensajito.js
@@ -1,3 +1,3 @@
 function mensaje(){
-    alert("Esta página ha sido creada con amor por bm0x. ❤️ \nCambios referentes a la versión 2.0:\n\n-Se Corrigieron varios errores de tipeo\n-se corrigió el número de teléfono y nombre de documento")
+    alert("Esta página ha sido creada con amor por bm0x. ❤️ \nCambios referentes a la versión 2.0:\n\n-Se Corrigieron varios errores de tipeo\n-se corrigió el número de teléfono y nombre de documento\n-Se corrigieron pequeños errores visuales, seguimos trabajando continuamente para solucionar los problemas relacionados con el modo responsivo")
 }
