7e81bfa99dc85af32876eab7efce8689b67df2cd
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2023-12-22 01:59:18 -0300

    Fix styling issues and update version number

--- js/mensajito.js
+++ js/mensajito.js
@@ -1,3 +1,3 @@
 function mensaje(){
-    alert("Esta página ha sido creada con amor por bm0x. ❤️  \n\nCambios referentes a la versión 2.0.1:\n\n-Se corrigió un pequeño error visual dentro de las tarjetas de las personas involucradas en el proyecto.")
+    alert("Esta página ha sido creada con amor por bm0x. ❤️  \n\nCambios referentes a la versión 2.0.2:\n\n-Se corrigió un pequeño error visual dentro de las tarjetas de las personas involucradas en el proyecto. \n-Se corrigió un problema visual interno del código el cual hacía que el elemento #cony se mostrara de mal manera en modo responsivo y en web \n-Se ajusta de mejor manera el responsive de las tarjetas de 'El Equipo'")
 }
